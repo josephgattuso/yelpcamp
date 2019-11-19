ce2f6659cb1ca4b75a908c2acef47400fa2e6337
Joseph Gattuso <joseph.gattuso@gmail.com>
2019-11-19 07:43:36 +0000

    feat: setup initial routes for express

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello world!');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.send('hello world!');
+});
+
+// Start the remote server
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log('The YelpCamp Server Has Started!');
+// });
+
+// Start the local server
+app.listen(3000, function() {
+  console.log('yelp-camp server started');
+});
