512c7e1875bd7a5bd002fb480999128896610b2b
Joseph Gattuso <joseph.gattuso@gmail.com>
2019-11-19 09:59:01 +0000

    feat: add comments routes

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
-  Campground = require('./models/campground');
+  Campground = require('./models/campground'),
+  Comment = require('./models/comment');
 
 require('dotenv').config();
+
+// seedDB = require("./seeds")
+// seedDB();
 
 // Set up MongoDB/mongoose using ATLAS to make it server-independent (code pulled from MongoDB atlas page )
 const mongoURI = process.env.databaseURL;
@@ -35,7 +39,7 @@ app.get('/campgrounds', function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render('index', { campgrounds: allCampgrounds });
+      res.render('campgrounds/index', { campgrounds: allCampgrounds });
     }
   });
 });
@@ -60,21 +64,61 @@ app.post('/campgrounds', function(req, res) {
 
 //NEW - show form to create new campground
 app.get('/campgrounds/new', function(req, res) {
-  res.render('new.ejs');
+  res.render('campgrounds/new');
 });
 
 // SHOW - shows more info about one campground
 app.get('/campgrounds/:id', function(req, res) {
   //find the campground with provided ID
-  Campground.findById(req.params.id).exec(function(err, foundCampground) {
+  Campground.findById(req.params.id)
+    .populate('comments')
+    .exec(function(err, foundCampground) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(foundCampground);
+        //render show template with that campground
+        res.render('campgrounds/show', { campground: foundCampground });
+      }
+    });
+});
+
+// ====================
+// COMMENTS ROUTES
+// ====================
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+  // find campground by id
+  Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
     } else {
-      console.log(foundCampground);
-      //render show template with that campground
-      res.render('show', { campground: foundCampground });
+      res.render('comments/new', { campground: campground });
     }
   });
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+  //lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      });
+    }
+  });
+  //create new comment
+  //connect new comment to campground
+  //redirect campground show page
 });
 
 // Start the remote server
