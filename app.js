9ab73161d00ac4c9b0ba3b0c9d59e19e8356fe13
Joseph Gattuso <joseph.gattuso@gmail.com>
2019-11-19 09:06:41 +0000

    feat: connect app to mongodb

--- app.js
+++ app.js
@@ -1,69 +1,103 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Set up MongoDB/mongoose using ATLAS to make it server-independent (code pulled from MongoDB atlas page )
+const mongoURI = process.env.databaseURL;
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    dbName: 'test',
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Connected to DB!');
+  })
+  .catch(err => {
+    console.log('ERROR:', err.message);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg'
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg'
-  },
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg'
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg'
-  },
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg'
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: 'https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg'
-  }
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//      {
+//          name: "Granite Hill",
+//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+
+//      },
+//      function(err, campground){
+//       if(err){
+//           console.log(err);
+//       } else {
+//           console.log("NEWLY CREATED CAMPGROUND: ");
+//           console.log(campground);
+//       }
+//     });
 
 app.get('/', function(req, res) {
   res.render('landing');
 });
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', function(req, res) {
-  res.render('campgrounds', { campgrounds: campgrounds });
+  // Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { campgrounds: allCampgrounds });
+    }
+  });
 });
 
+//CREATE - add new campground to DB
 app.post('/campgrounds', function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  //redirect back to campgrounds page
-  res.redirect('/campgrounds');
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
+  // Create a new campground and save to DB
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      //redirect back to campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
+//NEW - show form to create new campground
 app.get('/campgrounds/new', function(req, res) {
   res.render('new.ejs');
+});
+
+// SHOW - shows more info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+  //find the campground with provided ID
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render('show', { campground: foundCampground });
+    }
+  });
 });
 
 // Start the remote server
