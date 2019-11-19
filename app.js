0531e743a2bd8d77bb7d423c0adaa47c20265a1d
Joseph Gattuso <joseph.gattuso@gmail.com>
2019-11-19 08:01:13 +0000

    feat: add new campground form

--- app.js
+++ app.js
@@ -52,6 +52,20 @@ app.get('/campgrounds', function(req, res) {
   res.render('campgrounds', { campgrounds: campgrounds });
 });
 
+app.post('/campgrounds', function(req, res) {
+  // get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  //redirect back to campgrounds page
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new.ejs');
+});
+
 // Start the remote server
 // app.listen(process.env.PORT, process.env.IP, function() {
 //   console.log('The YelpCamp Server Has Started!');
