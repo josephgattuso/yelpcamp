a52dc31ed915690307766a607b78f47f276303ef
Joseph Gattuso <joseph.gattuso@gmail.com>
2019-11-21 12:32:25 +0000

    feat: associate campgrounds to users

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,25 +15,35 @@ router.get('/', function(req, res) {
 });
 
 //CREATE - add new campground to DB
-router.post('/', function(req, res) {
+router.post('/', isLoggedIn, function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = { name: name, image: image, description: desc };
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newCampground = {
+    name: name,
+    image: image,
+    description: desc,
+    author: author
+  };
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
       //redirect back to campgrounds page
+      console.log(newlyCreated);
       res.redirect('/campgrounds');
     }
   });
 });
 
 //NEW - show form to create new campground
-router.get('/new', function(req, res) {
+router.get('/new', isLoggedIn, function(req, res) {
   res.render('campgrounds/new');
 });
 
@@ -52,5 +62,13 @@ router.get('/:id', function(req, res) {
       }
     });
 });
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 module.exports = router;
